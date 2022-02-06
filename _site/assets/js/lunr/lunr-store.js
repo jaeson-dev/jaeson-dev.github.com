da03fac86668710a984abe9c5b9e77d08aca2427
Jaeson.dev <jaeson.dev@Jaesondevui-MacBookPro.local>
2022-02-06 20:46:02 +0900

    test posting

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "테스팅",
+        "excerpt":"이 포스트는 테스트용입니다.  ","categories": ["일상"],
+        "tags": ["daily"],
+        "url": "/%EC%9D%BC%EC%83%81/%EB%82%98%EC%9D%98-%EC%B2%AB-%EA%B9%83%EB%B8%94%EB%A1%9C%EA%B7%B8-%ED%8F%AC%EC%8A%A4%ED%8C%85/",
+        "teaser": null
+      }]
