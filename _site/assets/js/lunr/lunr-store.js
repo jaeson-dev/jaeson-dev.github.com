998dad0ffe95123cecbb511e3a2d8a5817c52b5c
jaeson-dev <jaeson.dev@gmail.com>
2022-02-08 18:51:43 +0900

    edit profile img and add email link

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["daily"],
         "url": "/%EC%9D%BC%EC%83%81/%EB%82%98%EC%9D%98-%EC%B2%AB-%EA%B9%83%EB%B8%94%EB%A1%9C%EA%B7%B8-%ED%8F%AC%EC%8A%A4%ED%8C%85/",
         "teaser": null
+      },{
+        "title": "Swift 공부 정리 목록 (1)",
+        "excerpt":"옵셔널 변수 및 unwrapping  구조체 종류(클래스, 스트럭트, enum)  제네릭  클로저(closure)   ","categories": [],
+        "tags": [],
+        "url": "/Swift-%EA%B3%B5%EB%B6%80-%EC%A0%95%EB%A6%AC-%EB%AA%A9%EB%A1%9D-(1)/",
+        "teaser": null
       }]
